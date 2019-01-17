908e66ff4cc866030847e8ef8999b872881e10e0
Rom Grk <romgrk.cc@gmail.com>
2019-01-17 15:18:08 -0500

    initial commit

---
+++ src/generate-tags.js
@@ -0,0 +1,217 @@
+/*
+ * generate-tags.js
+ */
+
+
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+const readFileToString = f => readFile(f).then(b => b.toString())
+const realpath = util.promisify(fs.realpath)
+const { parse, stringify } = require('scss-parser')
+
+/* util.inspect.defaultOptions = {
+ *   breakLength: 120,
+ *   depth: 4,
+ * } */
+
+module.exports = generateTags
+
+/*
+ * Definitions
+ */
+
+/**
+ * @typedef Tag
+ * @type {Object}
+ * @property {string} type
+ * @property {Object} node
+ * @property {Object[]} parents
+ * @property {string} filename
+ */
+
+
+/*
+ * Functions
+ */
+
+function generateTags(filename, options) {
+  return getPath(filename, { relative: options.relative })
+  .then(filepath =>
+    readFileToString(filepath)
+    .then(content => parseFile(filepath, content))
+  )
+  .then(tags => tags.map(tagToString))
+}
+
+/**
+ * @param {string} filename
+ * @param {string} content
+ * @return {Tag[]}
+ */
+function parseFile(filename, content) {
+  const tags = []
+
+  let ast = parse(content)
+
+  traverse(ast, (node, parent, parents) => {
+    if (node.type === 'declaration') {
+      const variable = getChild(node, 'property', 'variable')
+      if (variable !== undefined)
+        tags.push({ type: 'variable', node, parents, filename })
+    }
+
+    if (node.type === 'atrule' && node.value[0].value === 'mixin') {
+      const identifier = getChild(node, 'identifier')
+      if (identifier !== undefined)
+        tags.push({ type: 'mixin', node, parents, filename })
+    }
+
+    if (node.type === 'atrule' && node.value[0].value === 'function') {
+      const identifier = getChild(node, 'identifier')
+      if (identifier !== undefined)
+        tags.push({ type: 'function', node, parents, filename })
+    }
+
+    if (node.type === 'rule') {
+      tags.push({ type: 'rule', node, parents, filename })
+    }
+  })
+
+  return tags
+}
+
+/**
+ * @param {Tag} tag
+ * @returns {string}
+ */
+function tagToString(tag) {
+  const {type, node, parents, filename} = tag
+  switch (type) {
+    case 'variable': {
+      /* const parentRules =
+       *   parents
+       *   .filter(p => p.type !== 'stylesheet' && p.type !== 'block')
+       *   .map(getRuleText) */
+
+      const variable = getChild(node, 'property', 'variable')
+      const text = getValue(variable)
+      const line = variable.start.line
+      const letter = 'v'
+      // console.log(['variable', text, line])
+      return `${text}\t${filename}\t${line};"\t${letter}`
+    }
+    case 'mixin': {
+      const identifier = getChild(node, 'identifier')
+      const text = getValue(identifier)
+      const line = identifier.start.line
+      const letter = 'm'
+      // console.log(['mixin', text, line])
+      return `${text}\t${filename}\t${line};"\t${letter}`
+    }
+    case 'function': {
+      const identifier = getChild(node, 'identifier')
+      const text = getValue(identifier)
+      const line = identifier.start.line
+      const letter = 'f'
+      // console.log(['function', text, line])
+      return `${text}\t${filename}\t${line};"\t${letter}`
+    }
+    case 'rule': {
+      const parentRules =
+        parents
+        .filter(p => p.type !== 'stylesheet' && p.type !== 'block')
+        .map(getRuleText)
+
+      const text = parentRules.concat(getRuleText(node)).join(' ')
+      const line = node.start.line
+      const letter = 'r'
+      // console.log(['rule', text, line])
+      return `${text}\t${filename}\t${line};"\t${letter}`
+    }
+    default:
+      throw new Error('unreachable')
+  }
+}
+
+function getRuleText(rule) {
+  if (rule.type === 'atrule') {
+    return '@media ' + stringify(rule.value[2])
+  }
+  return rule.value[0].value.map(getValue).join('').trim().replace(/\s+/g, ' ')
+}
+
+function getValue(node) {
+  switch (node.type) {
+    case 'punctuation':
+    case 'space':
+    case 'identifier':
+    case 'operator':
+    case 'number':
+      return node.value
+
+    case 'pseudo_class':
+      return ':' + toString(node)
+
+    case 'function': {
+      const name = node.value[0].value[0].value
+      const args = node.value[1].value.map(getValue).join('')
+      return `:${name}(${args})`
+    }
+
+    case 'id':
+      return '#' + node.value.map(getValue).join('')
+
+    case 'class':
+      return '.' + node.value.map(getValue).join('')
+
+    case 'variable':
+      return '$' + node.value
+
+    default:
+      console.warning('Unexpected node', [node.type, node.value])
+      return node.value
+  }
+}
+
+function toString(node) {
+  if (typeof node.value === 'string')
+    return node.value
+  if (Array.isArray(node.value))
+    return node.value.map(toString).join('')
+  throw new Error(`unexpected node: ${node.type}: ${node.value}`)
+}
+
+function traverse(node, fn, parent = null, parents = []) {
+  if (parent !== null)
+    fn(node, parent, parents)
+
+  if (Array.isArray(node.value)) {
+    const nextParents = parents.concat(node)
+    node.value.forEach(v => {
+      traverse(v, fn, node, nextParents)
+    })
+  }
+}
+
+function getChild(node, ...types) {
+  let current = node
+  let type
+  while ((type = types.shift())) {
+    if (current === undefined)
+      return current
+    current = current.value.find(n => n.type === type)
+  }
+  return current
+}
+
+/*
+ * Helpers
+ */
+
+function getPath(filename, options = { relative: true }) {
+  return realpath(filename).then(filepath => options.relative ?
+    path.relative(process.cwd(), filepath) :
+    filepath)
+}
---
+++ src/index.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+/*
+ * index.js
+ */
+
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const exists = util.promisify(fs.exists)
+const readFile = util.promisify(fs.readFile)
+const readFileToString = f => readFile(f).then(b => b.toString())
+const writeFile = util.promisify(fs.writeFile)
+const realpath = util.promisify(fs.realpath)
+const stat = util.promisify(fs.stat)
+const program = require('commander')
+const micromatch = require('micromatch')
+const recursiveRead = require('recursive-readdir')
+
+const generateTags = require('./generate-tags.js')
+
+main()
+
+function main() {
+  const configFiles = [
+    path.join(process.env.HOME, '.ctags'),
+    path.join(process.env.HOME, '.ctags.d', 'excludes.ctags'),
+  ]
+
+  Promise.all(configFiles.map(readFileIfExists))
+  .then(configs =>
+    configs.filter(Boolean)
+           .map(c => c.split(/\s+/))
+           .reduce((acc, cur) => acc.concat(cur), []))
+  .then(configs => {
+
+    const argv =
+      process.argv.slice(0, 2).concat(
+        configs.concat(process.argv.slice(2))
+      )
+
+    program
+      .version('1.0.0')
+      .usage('[options] <files ...>')
+      .option('-f <file>', 'Tagfile ("-" for stdout)')
+      .option('-R, --recurse', 'Search in sub-directories')
+      .option('-a, --absolute', 'Use absolute paths')
+      .option('-k, --keepAll', 'By default, add *.scss to the exclude patterns. This prevents that.')
+      .option('    --exclude [pattern]', 'A repeatable value', collect, [])
+      .parse(argv)
+
+    const recurse = program.recurse
+    const relative = program.absolute ? false : true
+    const keepAll = program.keepAll
+    const includes = keepAll ? undefined : ['*.scss']
+    const excludes = program.exclude.concat()
+    const files = program.args
+    const output = program.F || 'tags'
+
+    const options = { excludes, includes, recurse }
+
+    // console.log(program)
+    // console.log({ argv, files, excludes, recurse, keepAll, relative })
+
+    collectFiles(files, options)
+    .then(filepaths => {
+      // console.log(filepaths)
+      return Promise.all(
+        filepaths.map(f => generateTags(f, { relative }))
+      )
+      .then(flatten)
+    })
+    .then(lines => lines.join('\n'))
+    .then(result => {
+      if (output === '-' || output === '--')
+        return console.log(result)
+
+      return writeFile(output, result)
+    })
+  })
+}
+
+
+
+/*
+ * Helpers
+ */
+
+function collectFiles(files, options) {
+  return Promise.all(files.map(f => realpath(f)))
+  .then(filepaths => filepaths.filter(f => !isExcluded(f, options)))
+  .then(filepaths => {
+    if (options.recurse) {
+      return Promise.all(filepaths.map(filepath =>
+        stat(filepath)
+        .then(s => {
+          if (s.isDirectory()) {
+            return recursiveRead(filepath, [(f, s) => isExcluded(f, options)])
+          }
+          return [filepath]
+        })
+      ))
+      .then(results => flatten(results))
+    }
+    return filepaths
+  })
+  .then(filepaths => options.includes ? filepaths.filter(f => isIncluded(f, options)) : filepaths)
+}
+
+function isExcluded(filepath, options) {
+  const filename = path.basename(filepath)
+
+  if (micromatch.some(filepath, options.excludes)
+   || micromatch.some(filename, options.excludes)) {
+    return true
+  }
+
+  return false
+}
+
+function isIncluded(filepath, options) {
+  const filename = path.basename(filepath)
+
+  if (!(micromatch.some(filepath, options.includes)
+     || micromatch.some(filename, options.includes))) {
+    return false
+  }
+
+  return true
+}
+
+function collect(val, memo) {
+  memo.push(val);
+  return memo;
+}
+
+function readFileIfExists(filepath) {
+  return exists(filepath).then(yes => yes ? readFileToString(filepath) : undefined)
+}
+
+function flatten(array) {
+  return array.reduce((acc, cur) => acc.concat(cur), [])
+}
